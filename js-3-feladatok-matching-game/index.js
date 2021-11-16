7071ff899c5a18a19dcc717fa235a82c384b398a
proeszti <kispothajas@gmail.com>
2021-11-16 21:50:46 +0100

    css trying

---
+++ js-3-feladatok-matching-game/index.js
@@ -0,0 +1,20 @@
+let divMain = document.createElement("div");
+let divRow1 = document.createElement("div");
+let divRow2 = document.createElement("div");
+document.body.appendChild(divMain);
+divMain.appendChild(divRow1);
+divMain.appendChild(divRow2);
+
+let elementCount = 5;
+for (let i = 0; i < elementCount.lenth; i++) {
+    let element1 = document.createElement("div");
+    document.divRow1.appendChild(element);
+    let span1 = document.createElement("span");
+    document.divRow1.appendChild(span1);
+}
+for (let i = 0; i < elementCount.length; i++) {
+    let element2 = document.createElement("div");
+    document.divRow2.appendChild(element);
+    let span2 = document.createElement("span");
+    document.divRow2.appendChild(span2);
+}
